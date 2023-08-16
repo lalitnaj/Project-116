d9fa550bbf2219b938a30170e00b3e10adb0eaaa
lalitnaj <86791474+lalitnaj@users.noreply.github.com>
2023-08-16 19:26:16 +0530

    Add files via upload

---
+++ pro-116/family tree/app.py
@@ -0,0 +1,53 @@
+# importing modules from flask library
+from flask import Flask , render_template
+
+# creating instance of class Flask, by providing __name__ keyword as argument
+app = Flask(__name__)
+
+# write the routes using decorator functions
+# default route or 'URL'
+@app.route("/")
+def home():
+
+    name = "Alex" # write your name
+    age = "18" # write your age
+
+    return render_template('index.html' , name = name , age = age)
+
+# define the route to father webpage
+@app.route("/")
+def home():
+
+    name = "Alexander" # write your name
+    age = "35" # write your age
+
+    return render_template('index.html' , name = name , age = age)
+
+# define the route to mother webpage
+@app.route("/")
+def home():
+
+    name = "Kelly" # write your name
+    age = "34" # write your age
+
+    return render_template('index.html' , name = name , age = age)
+
+
+# define the route to friends webpage
+@app.route("/")
+def home():
+
+    name = "George" # write your name
+    age = "18" # write your age
+
+    return render_template('index.html' , name = name , age = age)
+
+
+# add other routes, if you want
+
+
+
+
+# run the file
+if __name__  ==  '__main__':
+    app.run(debug=True)
